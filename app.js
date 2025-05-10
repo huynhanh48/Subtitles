0013dd5f4cd534f1f895f872b69c42f76fb67ca1
anh <anhvo482004@gmail.com>
2025-05-10 08:16:50 +0700

    Update library to version 1.1.0

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 import { getSubtitles } from './getSubtitles.js';
 
-getSubtitles({
-  videoID: 'RziNtIBkiLk',
-  preferredLangs: ['en'], 
-})
-  .then(({ lang, lines }) => {
-    console.log(`\nPhụ đề ngôn ngữ: ${lang}`);
-    lines.forEach(cap => {
-      console.log(`[${cap.start}s - ${cap.dur}s - seconds ${cap.seconds}] ${cap.text}`);
-    });
-  })
-  .catch(err => {
-    console.error(' Lỗi:', err.message);
-  });
+export  default getSubtitles;
